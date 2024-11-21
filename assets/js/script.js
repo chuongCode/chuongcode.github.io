4ca12deb74b408250b59f2a22dfdcf522032b99b
chuongCode <chuongrichard@gmail.com>
2024-11-21 00:36:39 -0500

    Fix whitespace, removal of deprecated styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,7 @@
 'use strict';
 
-
-
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
 
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
@@ -13,8 +9,6 @@ const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // sidebar toggle functionality for mobile
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
-
-
 
 // project variables
 const projectsItem = document.querySelectorAll("[data-project-item]");
@@ -55,8 +49,8 @@ projectsItem.forEach(item => {
         imgElement.src = content.getAttribute("data-src");
         imgElement.alt = "Project Image";
         imgElement.style.width = "80%";
-        imgElement.style.maxHeight = "455px"; // Restrict the maximum height
-        imgElement.style.objectFit = "cover"; // Crop the image to fit the specified area
+        imgElement.style.maxHeight = "455px";
+        imgElement.style.objectFit = "cover";
         imgElement.style.marginTop = "10px";
         imgElement.style.marginBottom = "10px";
         imgElement.style.borderRadius = "10px";
@@ -71,8 +65,8 @@ projectsItem.forEach(item => {
         videoElement.muted = true;
         videoElement.playsInline = true;
         videoElement.style.width = "80%";
-        videoElement.style.maxHeight = "455px"; // Restrict the maximum height
-        videoElement.style.objectFit = "cover"; // Crop the video to fit the specified area
+        videoElement.style.maxHeight = "455px";
+        videoElement.style.objectFit = "cover";
         videoElement.style.marginTop = "10px";
         videoElement.style.marginBottom = "10px";
         videoElement.style.borderRadius = "10px";
@@ -100,88 +94,10 @@ projectsItem.forEach(item => {
 modalCloseBtn.addEventListener("click", projectModalFunc);
 overlay.addEventListener("click", projectModalFunc);
 
-
-
-// custom select variables
-// const select = document.querySelector("[data-select]");
-// const selectItems = document.querySelectorAll("[data-select-item]");
-// const selectValue = document.querySelector("[data-selecct-value]");
-// const filterBtn = document.querySelectorAll("[data-filter-btn]");
-
-// select.addEventListener("click", function () { elementToggleFunc(this); });
-
-// add event in all select items
-// for (let i = 0; i < selectItems.length; i++) {
-//   selectItems[i].addEventListener("click", function () {
-
-//     let selectedValue = this.innerText.toLowerCase();
-//     selectValue.innerText = this.innerText;
-//     elementToggleFunc(select);
-//     filterFunc(selectedValue);
-
-//   });
-// }
-
-// filter variables
-// const filterItems = document.querySelectorAll("[data-filter-item]");
-
-// const filterFunc = function (selectedValue) {
-
-//   for (let i = 0; i < filterItems.length; i++) {
-
-//     if (selectedValue === "all") {
-//       filterItems[i].classList.add("active");
-//     } else if (selectedValue === filterItems[i].dataset.category) {
-//       filterItems[i].classList.add("active");
-//     } else {
-//       filterItems[i].classList.remove("active");
-//     }
-
-//   }
-
-// }
-
-// add event in all filter button items for large screen
-// let lastClickedBtn = filterBtn[0];
-
-// for (let i = 0; i < filterBtn.length; i++) {
-
-//   filterBtn[i].addEventListener("click", function () {
-
-//     let selectedValue = this.innerText.toLowerCase();
-//     selectValue.innerText = this.innerText;
-//     filterFunc(selectedValue);
-
-//     lastClickedBtn.classList.remove("active");
-//     this.classList.add("active");
-//     lastClickedBtn = this;
-
-//   });
-
-// }
-
-
-
 // contact form variables
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
 const formBtn = document.querySelector("[data-form-btn]");
-
-// add event to all form input field
-// for (let i = 0; i < formInputs.length; i++) {
-//   formInputs[i].addEventListener("input", function () {
-
-//     // check form validation
-//     if (form.checkValidity()) {
-//       formBtn.removeAttribute("disabled");
-//     } else {
-//       formBtn.setAttribute("disabled", "");
-//     }
-
-//   });
-// }
-
-
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
